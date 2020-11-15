5c8f4acf39b3718b6b6cd67d545c007b4afdaa7b
Riju Mukherjee <rmukh561@gmail.com>
2020-11-15 12:22:39 +0530

    *

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,6 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../utility';
-// import * as todoo from '../actions/todo';
 const initialState={
     todos:[]
 }
